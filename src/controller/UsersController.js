f395be8c157db61176992ecc707f5fb5688b05a5
luka <lukarodrigues25@gmail.com>
2025-02-20 11:03:15 -0400

    ✨ feat: bcrypt and INSERT
    add bcrypt and insert users in DB

--- src/controller/UsersController.js
+++ src/controller/UsersController.js
@@ -1,14 +1,23 @@
+const { hash } = require("bcryptjs");
 const AppError = require("../utils/AppError");
+const sqliteConnection = require("../database/sqlite");
 
 class UsersController {
-    create(request, response) {
-        const { name, email, password } = request.body;
+    async create(request, response) {
+        const { name, email, password, is_admin = false } = request.body;
+        const database = await sqliteConnection();
+        const checkUserExist = await database.get("SELECT * FROM users WHERE email = (?)", [email])
 
-        if (!name) {
-            throw new AppError("Nome é obrigatório!")
+        if (checkUserExist) {
+            throw new AppError("Este email já está em uso.")
         }
+        const hashedPassword = await hash(8, password);
 
-        response.status(201).json({ name, email, password })
+        await database.run(
+            "INSERT INTO users (name. email, password, is_admin) VALUES (?,?,?,?)",
+            [name, email, password, is_admin]
+        );
+
     }
 }
 
