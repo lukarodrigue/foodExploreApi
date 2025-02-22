3583928583b56b0365ef2c441c3855a97edb7cc5
luka rodrigues goncalves <lukarodrigues25@gmail.com>
2025-02-21 23:03:03 -0300

    ✨ feat: creating an ingredients table and refactoring table Dishes

--- knexfile.js
+++ knexfile.js
@@ -6,7 +6,10 @@ module.exports = {
     connection: {
       filename: path.resolve(__dirname, "src", "database", "database.db")
     },
-    migrations: { 
+    pool: {
+      afterCreate: (conn, cb) => conn.run("PRAGMA foreign_keys = ON", cb)
+    },
+    migrations: {
       directory: path.resolve(__dirname, "src", "database", "knex", "migrations")
     },
     useNullAsDefault: true
--- src/database/knex/migrations/20250222020008_createDishes.js
+++ src/database/knex/migrations/20250222020008_createDishes.js
@@ -5,12 +5,12 @@ exports.up = knex => knex.schema.createTable("dishes", table => {
     table.text("category").notNullable();
     table.decimal("price", 10, 2).notNullable();
     table.text("image").default(null);
-  
+
     table.integer("created_by").references("id").inTable("users");
     table.integer("updated_by").references("id").inTable("users");
-  
+
     table.timestamp("created_at").defaultTo(knex.fn.now());
     table.timestamp("updated_at").defaultTo(knex.fn.now());
-  });
-  
-  exports.down = knex => knex.schema.dropTable("dishes");
+});
+
+exports.down = knex => knex.schema.dropTable("dishes");
