ce981d3f46978571f8b2d10dea4cec0fa6092ecf
luka rodrigues goncalves <lukarodrigues25@gmail.com>
2025-02-21 23:18:02 -0300

    ✨ feat: registering dish and routes dishes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,13 @@
 const { Router } = require("express");
 
 const userRouter = require("./users.routes");
+const dishesRoutes = require("./dishes.routes");
+
 
 const routes = Router();
 routes.use("/users", userRouter);
+routes.use("/dishes", dishesRoutes);
+
+
 
 module.exports = routes;
