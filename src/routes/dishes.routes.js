381b51b292e7ef5cf55a29e05ef6912ad6d26823
luka rodrigues goncalves <lukarodrigues25@gmail.com>
2025-02-21 23:49:03 -0300

    ✨ feat: editing dishes

--- src/routes/dishes.routes.js
+++ src/routes/dishes.routes.js
@@ -8,5 +8,7 @@ const dishesController = new DishesController();
 
 dishesRoutes.post("/:user_id", dishesController.create);
 dishesRoutes.get("/:id", dishesController.show);
+dishesRoutes.delete("/:id", dishesController.delete);
+dishesRoutes.patch("/:id", dishesController.update);
 
 module.exports = dishesRoutes;
