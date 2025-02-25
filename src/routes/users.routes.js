5ceec347f7d791d60483e4f529e6b773db2fbfec
luka rodrigues goncalves <lukarodrigues25@gmail.com>
2025-02-24 23:53:42 -0300

    ✨ feat: add middleware

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,12 +1,14 @@
 const { Router } = require("express");
 
 const UsersController = require("../controller/UsersController");
+const ensureAuthenticated = require("../middlewares/ensureAuthenticated")
+
 
 const userRoutes = Router();
 
 const usersController = new UsersController();
 
 userRoutes.post("/", usersController.create);
-userRoutes.put("/:id", usersController.update);
+userRoutes.put("/", ensureAuthenticated, usersController.update);
 
 module.exports = userRoutes;
