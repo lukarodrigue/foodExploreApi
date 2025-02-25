88985caeea5e3cf3e1924e0c75fd519768dc3278
luka rodrigues goncalves <lukarodrigues25@gmail.com>
2025-02-24 23:21:34 -0300

    ✨ feat: index route

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,12 +2,12 @@ const { Router } = require("express");
 
 const userRouter = require("./users.routes");
 const dishesRoutes = require("./dishes.routes");
-
+const sessionsRouter = require("./sessions.routes");
 
 const routes = Router();
 routes.use("/users", userRouter);
 routes.use("/dishes", dishesRoutes);
-
+routes.use("/sessions", sessionsRouter);
 
 
 module.exports = routes;
